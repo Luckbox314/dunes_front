7b8723d8d5a215d5dfa16bac9a86c8daf5bad74b
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-07-06 23:50:06 -0400

    dunes textures

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { WS_URL } from '../config';
+import displacementMapImage from './dunes_height.png';
+import normalMapImage from './dunes_normals.png';
+import { color } from 'three/examples/jsm/nodes/Nodes.js';
 
 
 const scene = new THREE.Scene();
@@ -8,9 +11,111 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a grid helper
-const gridHelper = new THREE.GridHelper(2500, 125, 0x888888, 0x444444); // size, divisions, color1, color2
-scene.add(gridHelper);
+// Set up the camera position
+camera.position.set(1000, 0, 1000);
+camera.lookAt(0, 0, 0);
+// rotate camera 90 degrees on the look axis
+camera.rotation.z = Math.PI / 2;
+
+
+// Load displacement and normal map textures
+const displacementMap = new THREE.TextureLoader().load(displacementMapImage);
+const normalMap = new THREE.TextureLoader().load(normalMapImage);
+
+// Shader material for the dunes
+const material = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 1.0 },
+        displacementMap: { value: displacementMap },
+        normalMap: { value: normalMap },
+        color: { value: new THREE.Vector3(1, 1, 1) },
+        lightDirection: { value: new THREE.Vector3(1, 0.3, 0.3) },
+        skyColor: { value: new THREE.Vector3(134/255, 89/255, 66/255) }
+    },
+    vertexShader: `
+        uniform float time;
+        uniform sampler2D displacementMap;
+        uniform sampler2D normalMap;
+        varying vec2 vUv;
+
+        void main() {
+            vUv = texture2D(normalMap, uv).rg;
+            vec3 newPosition = position + normal * texture2D(displacementMap, uv).r * 60.0;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+        }
+    `,
+    fragmentShader: `
+        varying vec2 vUv;
+        uniform vec3 color;
+        uniform vec3 lightDirection;
+        uniform vec3 skyColor;
+
+        void main() {
+            vec3 normal = normalize(vec3(vUv - 0.5, 1.0));
+            float light = dot(normal, lightDirection);
+            vec3 color = mix(vec3(0.0, 0.0, 0.0), color, light + 0.5);
+            color = mix(color, skyColor, 1.0 - light);
+            gl_FragColor = vec4(color, 1.0);
+        }
+    `,
+    wireframe: false
+});
+
+// turn rgb(207, 161, 110) into a vector3
+
+material.uniforms.color.value = new THREE.Vector3(207/255, 161/255, 110/255);
+
+material.uniforms.lightDirection.value = new THREE.Vector3(1, 0.5, 0.5);
+// material.uniforms.lightDirection.value = new THREE.Vector3(1, 1, 1);
+
+// Shader material for the player
+const playerMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 1.0 },
+        displacementMap: { value: displacementMap },
+        color: { value: new THREE.Vector3(1.0, 0, 0) },
+        lightDirection: { value: new THREE.Vector3(1, -1, 0.5) },
+    },
+    vertexShader: `
+        uniform float time;
+        uniform sampler2D displacementMap;
+        varying vec2 vUv;
+        varying vec3 vNormal;
+
+        void main() {
+            vUv = uv;
+            vNormal = normal;
+            vec4 referencePosition = instanceMatrix * vec4( 0.0, 0.0, 0.0, 1.0);
+            referencePosition.x += 500.0;
+            referencePosition.y += 500.0;
+            float displacement = texture2D(displacementMap, referencePosition.xy / 1000.0).r * 60.0;
+            vec3 newPosition = position;
+            newPosition.z += displacement;
+            gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(newPosition, 1.0);
+        }
+    `,
+    fragmentShader: `
+        varying vec2 vUv;
+        varying vec3 vNormal;
+        uniform vec3 color;
+        uniform vec3 lightDirection;
+
+
+        void main() {
+            vec3 normal = normalize(vNormal);
+            float light = dot(normal, lightDirection);
+            vec3 color = mix(vec3(0.0, 0.0, 0.0), color, light + 0.5);
+            gl_FragColor = vec4(color, 1.0);
+        }
+    `,
+    wireframe: false
+});
+
+
+// Create a plane geometry for the dunes
+const dunesGeometry = new THREE.PlaneGeometry(1000, 1000, 100, 100); // width, height, widthSegments, heightSegments
+const dunes = new THREE.Mesh(dunesGeometry, material);
+scene.add(dunes);
 
 // Create a simple player representation (e.g., spheres)
 const players_server_positions = {};
@@ -18,28 +123,43 @@ const players = {};
 var local_player = {id: null, position: {x: 0, y: 0}};
 var local_player_position_cache = {x: 0, y: 0};
 
-function createPlayer(playerId, position) {
-    const geometry = new THREE.SphereGeometry(20, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ color: 0x9d3e3a });
-    const player = new THREE.Mesh(geometry, material);
-    player.position.set(position.x, 0 ,position.y);
-    players_server_positions[playerId] = {x: position.x, y: position.y};
-    scene.add(player);
-    players[playerId] = player;
+const playerGeometry = new THREE.SphereGeometry(20, 10, 10);
+const instancedGeometry = new THREE.InstancedBufferGeometry().copy(playerGeometry);
+
+const playerMesh = new THREE.InstancedMesh(instancedGeometry, playerMaterial, 300);
+playerMesh.count = 0;
+scene.add(playerMesh);
+
+// Function to add a player
+function addPlayer(playerId, position) {
+    players[playerId] = position;
+    updatePlayerAttributes();
 }
 
-function updatePlayerPosition(playerId) {
-    if (players[playerId]) {
-        players[playerId].position.lerp(
-            new THREE.Vector3(players_server_positions[playerId].x, 0, players_server_positions[playerId].y)
-            , 0.1
+// Function to remove a player
+function removePlayer(playerId) {
+    delete players[playerId];
+    updatePlayerAttributes();
+}
+
+// Function to update the custom position attribute
+function updatePlayerAttributes() {
+    Object.keys(players).forEach((playerId, index) => {
+        if (index >= playerMesh.count) {
+            playerMesh.count++;
+        }
+        const matrix = new THREE.Matrix4();
+        matrix.makeTranslation(
+            players[playerId].x,
+            players[playerId].y,
+            20
         );
-    }
+        playerMesh.setMatrixAt(index, matrix);
+    });
+    playerMesh.instanceMatrix.needsUpdate = true;
 }
 
-// Set up the camera position
-camera.position.set(1000, 1000, 1000);
-camera.lookAt(0, 0, 0);
+updatePlayerAttributes()
 
 // Variables to store mouse position and target world position
 let mouse = new THREE.Vector2();
@@ -56,36 +176,57 @@ function onMouseMove(event) {
     // Use raycaster to convert mouse position to world coordinates
     const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse, camera);
-    const intersects = raycaster.intersectObject(gridHelper);
+    const intersects = raycaster.intersectObject(dunes);
 
     if (intersects.length > 0) {
         target.copy(intersects[0].point);
     }
+
+    local_player.position.x = target.x;
+    local_player.position.y = target.y;
 }
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
 
-    // Move player towards target position
-    const speed = 0.1; // Adjust speed as needed
-    if (local_player.id) {
-        players[local_player.id].position.lerp(target, speed);
-        local_player.position.x = target.x;
-        local_player.position.y = target.z;
-    }
+
+    // Update the displacement shader time
+    material.uniforms.time.value += 0.01;
+    playerMaterial.uniforms.time.value += 0.01;
+
+
 
     // Update other players positions
     Object.keys(players_server_positions).forEach(playerId => {
-        if (local_player.id && playerId !== local_player.id) {
-            updatePlayerPosition(playerId);
+        if (players[playerId]) {
+            players[playerId].position.x = players_server_positions[playerId].x;
+            players[playerId].position.y = players_server_positions[playerId].y;
         }
     });
+
+    updatePlayerAttributes();
     
 
     renderer.render(scene, camera);
 }
 animate();
+
+
+
+// Handle window resize
+window.addEventListener('resize', () => {
+    camera.left = -window.innerWidth/2;
+    camera.right = window.innerWidth/2;
+    camera.top = window.innerHeight/2;
+    camera.bottom = -window.innerHeight/2;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// ##############################
+// #### Server communication ####
+// ##############################
 
 // Establish WebSocket connection
 const socket = new WebSocket(WS_URL);
@@ -100,8 +241,7 @@ socket.onmessage = (event) => {
         // delete players that are not in the new positions list
         Object.keys(players).forEach(playerId => {
             if (!data.positions[playerId]) {
-                scene.remove(players[playerId]);
-                delete players[playerId];
+                removePlayer(playerId);
                 // remove from players_server_positions
                 delete players_server_positions[playerId];
             }
@@ -114,7 +254,7 @@ socket.onmessage = (event) => {
                 players_server_positions[playerId].x = data.positions[playerId].x;
                 players_server_positions[playerId].y = data.positions[playerId].y;
             } else {
-                createPlayer(playerId, data.positions[playerId]);
+                addPlayer(playerId, data.positions[playerId]);
             }
         });
     }
@@ -122,7 +262,7 @@ socket.onmessage = (event) => {
         console.log('Identification:', data.id)
         local_player.id = data.id; 
         local_player.position = data.position;
-        createPlayer(data.id, data.position);
+        addPlayer(data.id, data.position);
 
     }
 };
@@ -133,8 +273,7 @@ setInterval(() => {
     }
     const data = JSON.stringify({ type: 'position-update', position: local_player.position});
     if (socket.readyState === WebSocket.OPEN) {
-        socket.send(data);  
-        console.log('sent', data);
+        socket.send(data);
     }
     local_player_position_cache.x = local_player.position.x;
     local_player_position_cache.y = local_player.position.y
@@ -143,14 +282,3 @@ setInterval(() => {
 socket.onclose = () => {
     console.log('Disconnected from the server');
 };
-
-
-// Handle window resize
-window.addEventListener('resize', () => {
-    camera.left = -window.innerWidth/2;
-    camera.right = window.innerWidth/2;
-    camera.top = window.innerHeight/2;
-    camera.bottom = -window.innerHeight/2;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,18 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
-            }
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
         ]
     },
     plugins: [
